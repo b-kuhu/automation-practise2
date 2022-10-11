e2b7f52fc31f40a10666b63d9c27efbb859022ca
b-kuhu <Kuhu Bhatnagar>
2022-10-11 11:09:51 +0530

    add page object config

--- codecept.conf.js
+++ codecept.conf.js
@@ -18,7 +18,12 @@ exports.config = {
     }
   },
   include: {
-    I: './steps_file.js'
+    I: './steps_file.js',
+    homePage:'./pages/home_page_test.js',
+    loginPage:'./pages/login_test.js',
+    registerPage:'./pages/registeration_test.js',
+    shopPage:'./pages/shop_test.js'
+
   },
   name: 'automation-practise'
 }
